223d8216bd680768b04a143ab6d765ce9289f1fa
Nicolai <nico8829@edu.eal.dk>
2016-09-22 09:15:25 +0200

    done

---
+++ 08_Orange_Tree/OrangeTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08_Orange_Tree
+{
+    internal class OrangeTree
+    {
+        public int Age;
+        public int Height;
+        public bool TreeAlive = true;
+        public int NumOranges;
+        public int OrangesEaten;
+
+        public OrangeTree(int age, int height)
+        {
+            this.Age = age;
+            this.Height = height;
+        }
+
+        internal void OneYearPasses()
+        {
+            Age = Age+1;
+            Height = Height + 2;
+            if(Age>=2)
+            {
+                NumOranges = NumOranges + 5;
+            }
+
+            if (Age >= 80) TreeAlive = false;
+        }
+
+        internal void EatOrange(int v)
+        {
+            if (NumOranges-v<= 0) throw new IndexOutOfRangeException("You can't eat an orange that isn't there!  There are 0 oranges available to eat");
+            else
+            {
+                NumOranges = NumOranges -v;
+                OrangesEaten = OrangesEaten + v;
+            }
+        }
+    }
+}
